36caa3c59ffe7a010b815183ca6e78b6d2831464
Kabir Zubaer <zubaer005@hotmail.com>
2021-05-23 01:11:29 +0600

    bounce program added

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+distance = 100
+for i in range(1, 11, 1):
+        new_distance = distance*(3/5)
+        print (i, round(new_distance,4))
+        distance=new_distance
