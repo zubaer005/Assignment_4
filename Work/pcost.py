5b50bee248157c4cb6f4946b609c0911b9cc4156
Kabir Zubaer <zubaer005@hotmail.com>
2021-05-24 02:12:46 +0600

    Pcost function added

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f).split(',')
-    for line in f:
-        row = line.split(',')
+def portfolio_cost(filename):
+    cost=0
+    with open('Data/portfolio.csv', 'rt') as f:
+        headers = next(f).split(',')
+        for line in f:
+            row = line.split(',') 
+            cost=cost+ (int(row[1])*float(row[2]))
         
- print(row)   
+    return cost 
+    
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
  
