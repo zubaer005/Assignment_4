9af920b98c3afb801ea7f1955c42eac0c3d9edb8
Kabir Zubaer <zubaer005@hotmail.com>
2021-05-30 01:00:03 +0600

    Exercise 2.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 def portfolio_cost(filename):
     cost=0
-    with open('Data/portfolio.csv', 'rt') as f:
-        headers = next(f).split(',')
-        for line in f:
-            row = line.split(',') 
-            cost=cost+ (int(row[1])*float(row[2]))
-        
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                cost += nshares * price
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
     return cost 
     
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost('Data/portfoliodate.csv')
 print('Total cost:', cost)
  
