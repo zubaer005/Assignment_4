49c04cc907dda418dc26cd0f5d4ad5554285759b
Kabir Zubaer <zubaer005@hotmail.com>
2021-05-29 10:43:39 +0600

    Exercise 2.12

--- Work/report.py
+++ Work/report.py
@@ -32,9 +32,19 @@ def read_prices(filename):
             except IndexError:
                 pass
     return prices
+
+def make_report(portfolio, prices):
+    report = []
+    for stock in portfolio:
+        price_now= prices[stock['name']]
+        change = price_now - stock['price']
+        summary = (stock['name'], stock['shares'], price_now, change)
+        report.append(summary)
+    return report
  
 portfolio = read_portfolio('Data/portfolio.csv')
 prices    = read_prices('Data/prices.csv')
+
 
 total =0.0 
 for s in portfolio:
@@ -49,3 +59,10 @@ for s in portfolio:
 
 print('Current value', total_value)
 print('Gain', total_value - total)
+
+report = make_report(portfolio, prices)
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for row in report:
+    print('%10s %10d  $%10.2f %10.2f' % row)
